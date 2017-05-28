1ca87da5e20388ad6d9f4c883bef070b89312ce3
nhutdev <tranvannhut4495@gmail.com>
2017-05-29 00:10:55 +0700

    fix query get many product color

--- vivu-data-access/services/product-color.js
+++ vivu-data-access/services/product-color.js
@@ -13,7 +13,8 @@
 const nodePg = require('node-pg');
 const exceptionHelper = require('../helpers/exception');
 const ProductColorAdapter = require('../adapters/product-color');
-
+const Product = require('vivu-common-api').models.Product;
+const helpers = require('node-helpers');
 
 class ProductColorService extends nodePg.services.Base {
 
@@ -37,10 +38,10 @@ class ProductColorService extends nodePg.services.Base {
 
   getManyByProduct(productId, result) {
     let opts = {};
-
+    let tableAlias = (new Product).tableAlias;
     return this.getAllCondition({
-      where: ['product_id = $1'],
-      args: [productId]
+      where: ['product_id = $1', `${tableAlias}.status = $2`],
+      args: [productId, helpers.Const.status.ACTIVE]
     }, opts, result);
 
   }
